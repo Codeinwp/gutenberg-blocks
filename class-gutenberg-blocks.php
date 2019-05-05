8270e03579c579cebc2defe930f0f83227ebba14
Hardeep Asrani <hardeepasrani@gmail.com>
2019-05-05 15:51:26 +0530

    Fix issue with css on reusable blocks
    Previously server-side CSS wasn't rendering on reusable blocks in
    WordPress. This should fix https://github.com/Codeinwp/gutenberg-blocks/issues/89

--- class-gutenberg-blocks.php
+++ class-gutenberg-blocks.php
@@ -500,6 +500,21 @@ if ( ! class_exists( '\ThemeIsle\GutenbergBlocks' ) ) {
 				if ( isset( $block['innerBlocks'] ) && ! empty( $block['innerBlocks'] ) && is_array( $block['innerBlocks'] ) ) {
 					$style .= $this->cycle_through_blocks( $block['innerBlocks'] );
 				}
+
+				if ( $block['blockName'] === 'core/block' && ! empty( $block['attrs']['ref'] ) ) {
+					$reusable_block = get_post( $block['attrs']['ref'] );
+
+					if ( ! $reusable_block || 'wp_block' !== $reusable_block->post_type ) {
+						return;
+					}
+
+					if ( 'publish' !== $reusable_block->post_status || ! empty( $reusable_block->post_password ) ) {
+						return;
+					}
+
+					$blocks = $this->parse_blocks( $reusable_block->post_content );
+					$style .= $this->cycle_through_blocks( $blocks );
+				}
 			}
 			return $style;
 		}
