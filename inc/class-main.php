7356bc9a8494e7a340bbad3f09e4bf6ce70e2990
Hardeep Asrani <hardeepasrani@gmail.com>
2020-03-18 19:14:35 +0530

    minor changes

--- inc/class-main.php
+++ inc/class-main.php
@@ -49,7 +49,7 @@ class Main {
 	public function init() {
 		if ( ! defined( 'THEMEISLE_BLOCKS_VERSION' ) ) {
 			define( 'THEMEISLE_BLOCKS_VERSION', '1.4.0' );
-			define( 'THEMEISLE_BLOCKS_DEV', true );
+			define( 'THEMEISLE_BLOCKS_DEV', false );
 		}
 
 		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
