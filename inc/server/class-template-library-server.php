6c8224dbd80610e4237d228500cea95523a15ffa
Hardeep Asrani <hardeepasrani@gmail.com>
2020-06-11 08:04:49 +0530

    Store Images locally when importing Templates
    Closes https://github.com/Codeinwp/gutenberg-blocks/issues/322

--- inc/server/class-template-library-server.php
+++ inc/server/class-template-library-server.php
@@ -155,7 +155,100 @@ class Template_Library_Server {
 		}
 
 		$obj = json_decode( $json );
+
+		if ( ! isset( $obj->__file ) || $obj->__file !== 'wp_export' || ! isset( $obj->content ) ) {
+			return new WP_Error( 'invalid_json', __( 'Invalid JSON file.', 'textdomain' ) );
+		}
+
+		$regex = '/https?:\/\/\S+(?:png|jpg|jpeg|gif|webp)/';
+		preg_match_all( $regex, $obj->content, $images, PREG_SET_ORDER, 0 );
+
+		if ( count( $images ) <= 1 ) {
+			foreach( $images as $image ) {
+				$image = $image[0];
+
+				$value = $this->import_image( $image );
+
+				if ( $value ) {
+					$obj->content = str_replace( $image, $value, $obj->content );
+				}
+			}
+		}
+
 		return rest_ensure_response( $obj );
+	}
+
+	/**
+	 * Get image from Media Library by hash
+	 *
+	 * @param $url Image URL.
+	 *
+	 * @return string
+	 */
+	public function get_saved_image( $url ) {
+		global $wpdb;
+
+		$post_id = $wpdb->get_var(
+			$wpdb->prepare(
+				'SELECT `post_id` FROM `' . $wpdb->postmeta . '`
+					WHERE `meta_key` = \'_themeisle_blocks_image_hash\'
+						AND `meta_value` = %s
+				;',
+				sha1( $url )
+			)
+		);
+
+		if ( $post_id ) {
+			return $post_id;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Upload image to Media Library
+	 *
+	 * @param $url Image URL.
+	 *
+	 * @return string
+	 */
+	public function import_image( $url ) {
+		$saved_image = $this->get_saved_image( $url );
+
+		if ( $saved_image ) {
+			return wp_get_attachment_url( $saved_image );;
+		}
+
+		if ( ! function_exists( 'media_handle_sideload' ) ) {
+			require_once ABSPATH . '/wp-admin/includes/file.php';
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+			require_once ABSPATH . 'wp-admin/includes/media.php';
+		}
+
+		$tmp = download_url( $url );
+
+		$file_array = array(
+			'name' => basename( $url ),
+			'tmp_name' => $tmp
+		);
+
+		if ( is_wp_error( $tmp ) ) {
+			@unlink( $file_array[ 'tmp_name' ] );
+			return $tmp;
+		}
+
+		$id = media_handle_sideload( $file_array );
+
+		if ( is_wp_error( $id ) ) {
+			@unlink( $file_array['tmp_name'] );
+			return $id;
+		}
+
+		update_post_meta( $id, '_themeisle_blocks_image_hash', sha1( $url ) );
+
+		$value = wp_get_attachment_url( $id );
+
+		return $value;
 	}
 
 	/**
