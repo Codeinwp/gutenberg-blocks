7419bca1c15d423924a933c44d69adbade379dcf
Hardeep Asrani <hardeepasrani@gmail.com>
2020-07-16 02:36:09 +0530

    Fix phpcs error

--- inc/css/blocks/class-button-css.php
+++ inc/css/blocks/class-button-css.php
@@ -62,22 +62,22 @@ class Button_CSS extends Base_CSS {
 			$style .= '}' . "\n \n";
 
 			$style .= '#' . $attr['id'] . ' .wp-block-button__link:hover {' . "\n";
-				if ( isset( $attr['hoverColor'] ) && ! empty( $attr['hoverColor'] ) ) {
-					$style .= '	color: ' . $this->get_attr_value( ( isset( $attr['hoverColor'] ) ? $attr['hoverColor'] : null ) ) . ';' . "\n";
-				}
+			if ( isset( $attr['hoverColor'] ) && ! empty( $attr['hoverColor'] ) ) {
+				$style .= '	color: ' . $this->get_attr_value( ( isset( $attr['hoverColor'] ) ? $attr['hoverColor'] : null ) ) . ';' . "\n";
+			}
 	
-				if ( ( isset( $attr['hoverBackground'] ) && ! empty( $attr['hoverBackground'] ) ) || ( isset( $attr['hoverBackgroundGradient'] ) && ! empty( $attr['hoverBackgroundGradient'] ) ) ) {
-					$style .= '	background: ' . $this->get_attr_value( ( isset( $attr['hoverBackground'] ) ? $attr['hoverBackground'] : $attr['hoverBackgroundGradient'] ) ) . ';' . "\n";
-				}
+			if ( ( isset( $attr['hoverBackground'] ) && ! empty( $attr['hoverBackground'] ) ) || ( isset( $attr['hoverBackgroundGradient'] ) && ! empty( $attr['hoverBackgroundGradient'] ) ) ) {
+				$style .= '	background: ' . $this->get_attr_value( ( isset( $attr['hoverBackground'] ) ? $attr['hoverBackground'] : $attr['hoverBackgroundGradient'] ) ) . ';' . "\n";
+			}
 	
-				if ( isset( $attr['hoverBorder'] ) && ! empty( $attr['hoverBorder'] ) ) {
-					$style .= '	border-color: ' . $this->get_attr_value( ( isset( $attr['hoverBorder'] ) ? $attr['hoverBorder'] : null ) ) . ';' . "\n";
-				}
+			if ( isset( $attr['hoverBorder'] ) && ! empty( $attr['hoverBorder'] ) ) {
+				$style .= '	border-color: ' . $this->get_attr_value( ( isset( $attr['hoverBorder'] ) ? $attr['hoverBorder'] : null ) ) . ';' . "\n";
+			}
 
-				if ( isset( $attr['boxShadow'] ) && true === $attr['boxShadow'] ) {
-					$style .= '	box-shadow: ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowHorizontal'] ) ? $attr['hoverBoxShadowHorizontal'] : null ), 0 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowVertical'] ) ? $attr['hoverBoxShadowVertical'] : null ), 0 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowBlur'] ) ? $attr['hoverBoxShadowBlur'] : null ), 5 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowSpread'] ) ? $attr['hoverBoxShadowSpread'] : null ), 1 ) . 'px ' . $this->hex2rgba( $this->get_attr_value( ( isset( $attr['hoverBoxShadowColor'] ) ? $attr['hoverBoxShadowColor'] : null ), '#000' ), $this->get_attr_value( ( isset( $attr['hoverBoxShadowColorOpacity'] ) ? $attr['hoverBoxShadowColorOpacity'] : null ), '50' ) / 100 ) . ';' . "\n";
-				}
-				$style .= '}' . "\n \n";
+			if ( isset( $attr['boxShadow'] ) && true === $attr['boxShadow'] ) {
+				$style .= '	box-shadow: ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowHorizontal'] ) ? $attr['hoverBoxShadowHorizontal'] : null ), 0 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowVertical'] ) ? $attr['hoverBoxShadowVertical'] : null ), 0 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowBlur'] ) ? $attr['hoverBoxShadowBlur'] : null ), 5 ) . 'px ' . $this->get_attr_value( ( isset( $attr['hoverBoxShadowSpread'] ) ? $attr['hoverBoxShadowSpread'] : null ), 1 ) . 'px ' . $this->hex2rgba( $this->get_attr_value( ( isset( $attr['hoverBoxShadowColor'] ) ? $attr['hoverBoxShadowColor'] : null ), '#000' ), $this->get_attr_value( ( isset( $attr['hoverBoxShadowColorOpacity'] ) ? $attr['hoverBoxShadowColorOpacity'] : null ), '50' ) / 100 ) . ';' . "\n";
+			}
+			$style .= '}' . "\n \n";
 		}
 
 		return $style;
