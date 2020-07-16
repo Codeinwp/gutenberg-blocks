bd601c894e589f70eef198aeb93a6a6ba09e0165
Soare Robert <soarerobert17@gmail.com>
2020-07-16 09:28:50 +0300

    Fixing the import statement

--- src/blocks/slider/edit.js
+++ src/blocks/slider/edit.js
@@ -21,7 +21,7 @@ const {
 
 import Placeholder from './placeholder.js';
 import Inspector from './inspector.js';
-import Slide from './components/slide.js';
+import Slide from './components/Slide.js';
 import SliderControls from './components/slider-controls.js';
 
 const IDs = [];
