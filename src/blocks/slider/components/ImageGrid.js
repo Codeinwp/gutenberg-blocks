b238f1c75bcdd79a1f6ea0c7f0005cd80c15a91a
Soare Robert <soarerobert17@gmail.com>
2020-07-17 11:25:50 +0300

    Image Grid Prototype v1

--- src/blocks/slider/components/ImageGrid.js
+++ src/blocks/slider/components/ImageGrid.js
@@ -5,37 +5,44 @@ import arrayMove from 'array-move';
 
 import {
 	SortableContainer,
-	SortableElement,
-	SortableHandle
+    SortableElement,
+    SortableHandle
 } from 'react-sortable-hoc';
 
 const SortableItem = SortableElement( ({ url }) => {
     return (
-            <img src={ url } style={{ width: "100px", height: "100px", padding: "6px" }} />
-    )    
+        <img src={ url } />
+        )    
 });
 
-const SortableList = SortableContainer( ({ images }) => {
+const SortableList = SortableContainer( ({ images, className }) => {
     return (
-        <div style={{ display: "content"}}>
+        <div className={className}>
             {
-                images.map( ({ id, url }, index) => (
-                    <SortableItem key={`image-${id}`} index={index} url={ url } /> 
+                images.map(({ id, url }, index) => (
+                    <SortableItem key={`image-${id}`} index={index} url={url} />
                 ))
             }
+            <button onClick={() => wp.media.frame.open()}>Click Me</button>
         </div>
     )
 } );
 
-const GridList = ({ attributes, onSelectImages }) => {
+
+const GridList = ({ attributes, onSelectImages, className }) => {
+
     
+    
+
     const onSortEnd = ({ oldIndex, newIndex })  => {
         const images = arrayMove( attributes.images, oldIndex, newIndex );
-		onSelectImages( images.filter( image => image !== undefined) );
+		onSelectImages( images );
     };
+
+
     
     return (
-        <SortableList images={ attributes.images } onSortEnd={ onSortEnd } axis="xy"/>
+        <SortableList className={className} images={attributes.images} onSortEnd={onSortEnd} axis="xy" />
     );
 }
 
