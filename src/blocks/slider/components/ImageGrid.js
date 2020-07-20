1563ee39308ae492f10f9afac37e11d5b86e671b
Soare Robert <soarerobert17@gmail.com>
2020-07-20 16:34:47 +0300

    Slide Multi Select Prototype 1

--- src/blocks/slider/components/ImageGrid.js
+++ src/blocks/slider/components/ImageGrid.js
@@ -22,37 +22,48 @@ const { __ } = wp.i18n;
 
 const { Button } = wp.components;
 
-const SortableItem = SortableElement( ({ url }) => (
+const { useState } = wp.element;
+
+const SortableItem = SortableElement( ({ onClick, value }) => (
 	<div
 		className="wp-block-themeisle-blocks-slider-images-grid__image"
 		tabIndex="0"
-		style={ {
-			backgroundImage: `url( ' ${ url } ' )`
-		} }
+		onClick={() => onClick( value )}
+		onKeyPress={( event ) => {
+			if ( event.which === ENTER_KEY ) {
+				onClick( value );
+			}
+		}}
+		style={{
+			backgroundImage: `url( ' ${value.url} ' )`
+		}}
 	/>
 ) );
 
 const SortableList = SortableContainer( ({
-	images,
+	items,
 	className,
+	onItemSelect,
 	open
 }) => {
+	console.log( items );
 	return (
 		<div
-			className={ className }
-			tabIndex={ 0 }
+			className={className}
+			tabIndex={0}
 		>
-			{ images.map( ({ id, url }, index ) => (
+			{items.map( ( value, index ) => (
 				<SortableItem
-					key={ `image-${ id }` }
+					key={ `image-${value.id}` }
 					index={ index }
-					url={ url }
+					value={ value }
+					onClick={ onItemSelect }
 				/>
-			) ) }
+			) )}
 
 			<Button
-				label={ __( 'Add Images' ) }
-				icon={ <Icon icon={ plus } /> }
+				label={__( 'Add Images' )}
+				icon={<Icon icon={ plus } />}
 				isPrimary
 				onClick={ open }
 			/>
@@ -65,23 +76,126 @@ const GridList = ({
 	onSelectImages,
 	open
 }) => {
+
+	const [ state, setState ] = useState({
+		selectedItems: [],
+		isSorting: false,
+		sortingItemKey: null,
+		items: attributes.images
+	});
+
+	console.log( state );
+
+	const filterItems = ( value ) => {
+		const { selectedItems, sortingItemKey, isSorting } = state;
+
+		// Do not hide the ghost of the element currently being sorted
+		if ( sortingItemKey === value ) {
+			return true;
+		}
+
+		// Hide the other items that are selected
+		if ( isSorting && selectedItems.includes( value ) ) {
+			return false;
+		}
+
+		// Do not hide any other items
+		return true;
+	};
+
+
+	const handleUpdateBeforeSortStart = ({ index }) => {
+		return new Promise( ( resolve ) =>
+			setState(
+				( prevState ) => {
+					prevState.sortingItemKey = prevState.items[index];
+					prevState.isSorting = true;
+					return { ...prevState };
+				},
+				resolve
+			)
+		);
+	};
+
 	const onSortEnd = ({
 		oldIndex,
 		newIndex
 	}) => {
-		const images = arrayMove( attributes.images, oldIndex, newIndex );
-		onSelectImages( images.filter( image => undefined !== image ) );
+		const { selectedItems } = state;
+		let newItems;
+
+		if ( selectedItems.length ) {
+			const items = state.items.filter(
+				( value ) => ! selectedItems.includes( value )
+			);
+
+			newItems = [
+				...items.slice( 0, newIndex ),
+				...selectedItems,
+				...items.slice( newIndex, items.length )
+			];
+		} else {
+			newItems = arrayMove( state.items, oldIndex, newIndex );
+		}
+
+		console.log( newItems );
+
+		setState({
+			items: newItems,
+			isSorting: false,
+			sortingItemKey: null,
+			selectedItems: []
+		});
+
+		onSelectImages( newItems );
+	};
+
+	const handleItemSelect = ( item ) => {
+		setState( ( prevState ) => {
+			if ( prevState.selectedItems.includes( item ) ) {
+				prevState.selectedItems = prevState.selectedItems.filter( ( value ) => value !== item );
+			} else {
+				prevState.selectedItems = [ ...prevState.selectedItems, item ];
+			}
+			return {
+				...prevState
+			};
+		});
+	};
+
+	const handleShouldCancelStart = ( event ) => {
+		const { items, selectedItems } = state;
+		const item = items[event.target.sortableInfo.index];
+
+		// Never cancel start if there are no selected items
+		if ( ! selectedItems.length ) {
+			return false;
+		}
+
+		// If there are selected items, we want to cancel sorting
+		// from starting when dragging elements that are not selected
+		return ! selectedItems.includes( item );
 	};
 
 	return (
 		<SortableList
-			className={ classnames(
+			className={classnames(
 				'wp-block-themeisle-blocks-slider-images-grid',
 				{ 'is-single': 1 === attributes.images.length }
-			) }
-			images={ attributes.images }
+			)}
+
+			items={state.items.filter( filterItems )}
+			isSorting={state.isSorting}
+			sortingItemKey={state.sortingItemKey}
+			selectedItems={state.selectedItems}
+			onItemSelect={ handleItemSelect }
+			shouldCancelStart={ handleShouldCancelStart }
+			updateBeforeSortStart={ handleUpdateBeforeSortStart }
+
+			//onSortStart={ onSortStart }
 			onSortEnd={ onSortEnd }
-			open={ open }
+			distance={0}
+			open={open}
 			axis="xy"
 		/>
 	);
