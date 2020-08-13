7281f8c7a7fe531f01346281b6a5dcb357508329
Pedro <soarerobert17@gmail.com>
2020-08-13 15:08:17 +0000

    Progress Bar v0.5.1

--- src/blocks/progress-bar/save.js
+++ src/blocks/progress-bar/save.js
@@ -2,40 +2,37 @@ import classname from 'classnames';
 
 
 const { RichText } = wp.blockEditor;
-const { Fragment } = wp.element;
 
 const ProgressBar = ({  attributes }) => {
 
 	const { text, value, progressColor, backgroundColor, borderRadius, height, animated, hideValue } = attributes;
 
 	return (
-		<Fragment>
-			<div className="wp-themeisle-block-progress-bar">
-				<div className="wp-themeisle-block-progress-bar__content">
-					<RichText.Content
-						tagName="p"
-						className="wp-themeisle-block-progress-bar__title"
-						value={ text }
-					/>
-					<span className="wp-themeisle-block-progress-bar__value">
-						{
-							hideValue && `${value}%`
-						}
-					</span>
-				</div>
+		<div className="wp-themeisle-block-progress-bar">
+			<div className="wp-themeisle-block-progress-bar__content">
+				<RichText.Content
+					tagName="p"
+					className="wp-themeisle-block-progress-bar__title"
+					value={ text }
+				/>
+				<span className="wp-themeisle-block-progress-bar__value">
+					{
+						hideValue && `${value}%`
+					}
+				</span>
+			</div>
+			<div
+				className='wp-themeisle-block-progress-bar__bar'
+				style={{ backgroundColor, borderRadius: `${borderRadius}px`, height: `${height}px` }}>
 				<div
-					className='wp-themeisle-block-progress-bar__bar'
-					style={{ backgroundColor, borderRadius: `${borderRadius}px`, height: `${height}px` }}>
-					<div
-						className={
-							classname( 'wp-themeisle-block-progress-bar__progress', { 'has-animation': animated }, { 'has-no-animation': ! animated })
-						}
-						style={{ backgroundColor: progressColor, '--width': `${value}%`, maxWidth: `${value}%`, borderRadius: `${borderRadius}px` }}>
+					className={
+						classname( 'wp-themeisle-block-progress-bar__progress', { 'has-animation': animated }, { 'has-no-animation': ! animated })
+					}
+					style={{ backgroundColor: progressColor, '--width': `${value}%`, maxWidth: `${value}%`, borderRadius: `${borderRadius}px` }}>
 
-					</div>
 				</div>
 			</div>
-		</Fragment>
+		</div>
 	);
 };
 
