bdae470daa8c291b4c6b88a96822525176af5e10
Robert <soarerobert17@gmail.com>
2021-02-09 08:36:10 +0000

    webpack config

---
+++ src/frontend/leaflet-map/index.js
@@ -0,0 +1,8 @@
+/**
+ * WordPress dependencies
+ */
+const domReady = wp.domReady;
+
+domReady( () => {
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 
 module.exports = {
+	stats: 'minimal',
 	mode: NODE_ENV,
 	entry: {
 		blocks: [
