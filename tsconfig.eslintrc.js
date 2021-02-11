bba3571109b94ec6dada7269377cf6c0d46a6aad
Robert <soarerobert17@gmail.com>
2021-02-11 16:40:47 +0000

    Linter

---
+++ tsconfig.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true // Allows for the parsing of JSX
+		}
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'wordpress'
+	],
+
+	rules: {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+
+};
