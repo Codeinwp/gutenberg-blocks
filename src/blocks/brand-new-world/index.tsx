a7d0494dec31eab50662945c5a2bae73e05f0aa8
S. Robert <soarerobert17@gmail.com>
2021-03-05 11:14:19 +0000

    test hooks 2

--- src/blocks/brand-new-world/index.tsx
+++ src/blocks/brand-new-world/index.tsx
@@ -9,11 +9,7 @@ import edit from './edit';
 import save from './save';
 import attributes from './attributes';
 
-const result = registerBlockType( 'themeisle-blocks/typescript-text', {
-	title: __( 'Test Typescript' ),
-	description: __( 'Put the logo maker on the site.' ),
-	category: 'widgets',
-	icon: 'smiley',
+const result = registerBlockType( 'themeisle-blocks/typescript-text', { title: __( 'Test Typescript' ), description: __( 'Put the logo maker on the site.' ), category: 'widgets', icon: 'smiley',
 	attributes: attributes,
 	edit: edit,
 	save: save
