c38d1eec90fe88f6f8f6e0b452eb239a4eab05e4
S. Robert <soarerobert17@gmail.com>
2021-03-12 08:23:32 +0000

    Hook refactor

--- src/helpers/blocks-helpers.js
+++ src/helpers/blocks-helpers.js
@@ -10,6 +10,31 @@ const {
 } = lodash;
 
 /**
+ * Utiliy function for creating a function that add the gobal defaults values to the block's attribute value.
+ * @param {*} attributes The block's attributes provided by WordPress
+ * @param {*} setAttributes The block's attributes update function provided by WordPress
+ * @param {*} name The block's name provided by WordPress
+ * @param {*} defaultAttributes The default attributes of the block.
+ * @returns {function} Function that can be called to add the global defaults value to the block's attribute value.
+ */
+export const addGlobalDefaults = ( attributes, setAttributes, name, defaultAttributes ) => () => {
+
+	// Check if the global default are available.
+	if ( undefined !== window.themeisleGutenberg.globalDefaults && ! isEqual( defaults[name], window.themeisleGutenberg.globalDefaults[name]) ) {
+		const defaultGlobalAttrs = { ...window.themeisleGutenberg.globalDefaults[name] };
+
+		const attrs = Object.keys( defaultGlobalAttrs )
+			.filter( attr => attributes[ attr ] === defaultAttributes[ attr ]?.default ) // Keep only the properties with the default value.
+			.reduce( ( attrs, attr ) => {
+				attrs[ attr ] = defaultGlobalAttrs[ attr ];
+				return attrs;
+			}, {});
+
+		setAttributes({ ...attrs });
+	}
+};
+
+/**
  * Utiliy function for creating the block's id and applying the Global Defaults on initialization.
  * @param {*} attributes The block's attributes provided by WordPress
  * @param {function} setAttributes The block's attributes update function provided by WordPress
@@ -20,28 +45,11 @@ const {
  */
 export const initBlock = ( attributes, setAttributes, clientId, idPrefix, name, defaultAttributes ) => {
 
-	const addGlobalDefaultOnInitialize = () => {
-
-		// Check if the global default are available.
-		if ( undefined !== window.themeisleGutenberg.globalDefaults && ! isEqual( defaults[name], window.themeisleGutenberg.globalDefaults[name]) ) {
-			const defaultGlobalAttrs = { ...window.themeisleGutenberg.globalDefaults[name] };
-
-			const attrs = Object.keys( defaultGlobalAttrs )
-				.filter( attr => attributes[ attr ] === defaultAttributes[ attr ]?.default ) // Keep only the properties with the default value.
-				.reduce( ( attrs, attr ) => {
-					attrs[ attr ] = defaultGlobalAttrs[ attr ];
-					return attrs;
-				}, {});
-
-			setAttributes({ ...attrs });
-		}
-	};
-
-	const id = useId( idPrefix, clientId, attributes.id, addGlobalDefaultOnInitialize );
+	const id = useId( idPrefix, clientId, attributes.id, addGlobalDefaults( attributes, setAttributes, name, defaultAttributes ) );
 
 	useEffect( () => {
 		if ( id !== attributes.id ) {
-			console.info( `Set new id to ${id}. Old id was ${attributes.id} for the block with the 'clientId' equal to ${clientId}. Global scope is: ${window.themeisleGutenberg.blockIDs}` );
+			console.info( `Set new id to ${id}. Old id was ${attributes.id} for the block with the 'clientId' equal to ${clientId}. Global scope is: `, window.themeisleGutenberg.blockIDs );
 			setAttributes({
 				id: id
 			});
