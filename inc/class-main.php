26f4ee36ca14a5044c18ec3cc9a6d2c39f169faa
Hardeep Asrani <hardeepasrani@gmail.com>
2021-05-24 15:00:33 +0530

    Rebase (#682)
    * release: version 1.6.6
    
    - Fix Product Review Block conflict with WooCommerce
    
    * chore(release): 1.6.6
    
    ##### [Version 1.6.6](https://github.com/Codeinwp/gutenberg-blocks/compare/v1.6.5...v1.6.6) (2021-05-24)
    
    - Fix Product Review Block conflict with WooCommerce
    
    Co-authored-by: themeisle[bot] <marius+gitbot@themeisle.com>

--- inc/class-main.php
+++ inc/class-main.php
@@ -101,7 +101,7 @@ class Main {
 	 */
 	public function init() {
 		if ( ! defined( 'THEMEISLE_BLOCKS_VERSION' ) ) {
-			define( 'THEMEISLE_BLOCKS_VERSION', '1.6.5' );
+			define( 'THEMEISLE_BLOCKS_VERSION', '1.6.6' );
 			define( 'THEMEISLE_BLOCKS_DEV', false );
 		}
 
