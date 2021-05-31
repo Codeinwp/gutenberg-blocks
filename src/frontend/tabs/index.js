ae30d536ac4628fb395c87e7e78de2d5d5ce9c9a
S. Robert <soarerobert17@gmail.com>
2021-05-31 09:51:50 +0000

    Build

---
+++ src/frontend/tabs/index.js
@@ -0,0 +1,91 @@
+/**
+ * WordPress dependencies
+ */
+const domReady = wp.domReady;
+
+/**
+ * Active or deactivate a tab
+ * @param {HTMLElement} tab
+ * @param {boolean} isActive
+ */
+const setTabStatus = ( tab, isActive = false ) => {
+	if ( ! tab ) {
+		return;
+	}
+	if ( isActive ) {
+		tab.classList.add( 'active' );
+	} else {
+		tab.classList.remove( 'active' );
+	}
+};
+
+/**
+ * Active the first tab
+ * @param {NodeListOf<Element>} headers List of headers
+ * @param {NodeListOf<Element>} tabs List of the tab
+ */
+const activateFirstTab = ( headers, tabs ) => {
+	console.log( 'Active first tabs' );
+	if ( 0 < headers.length ) {
+		headers[0].classList.add( 'active' );
+	}
+
+	if ( 0 < tabs.length ) {
+		const firstTab = tabs[0];
+		firstTab.querySelector( '.wp-block-themeisle-blocks-tabs-item-content' )?.classList.add( 'active' );
+	}
+};
+
+domReady( () => {
+	const tabsBlocks = document.querySelectorAll( '.wp-block-themeisle-blocks-tabs' );
+
+	/**
+	 * Used for desktop
+	 * Set activation function for the headers of the parent component
+	 */
+	tabsBlocks.forEach( tabsBlock => {
+		const headers = tabsBlock.querySelectorAll( '.wp-block-themeisle-blocks-tabs-header' );
+		const setHeadersInactive = () => {
+			headers.forEach( header => {
+				const target = tabsBlock.querySelector( `#${header.dataset.tabId} .wp-block-themeisle-blocks-tabs-item-content` );
+				setTabStatus( target );
+				header.classList.remove( 'active' );
+			});
+		};
+
+		headers.forEach( header => {
+			header.addEventListener( 'click', () => {
+				setHeadersInactive();
+
+				// Active the tab
+				const target = tabsBlock.querySelector( `#${header.dataset.tabId} .wp-block-themeisle-blocks-tabs-item-content` );
+				setTabStatus( target, true );
+				header.classList.add( 'active' );
+			});
+		});
+
+		const tabs = tabsBlock.querySelectorAll( '.wp-block-themeisle-blocks-tabs-item' );
+
+		/**
+		 * Used for mobile
+		 * Set activation function for each tab
+		 */
+		tabs.forEach( tab => {
+			const header = tab.querySelector( '.wp-block-themeisle-blocks-tabs-item-header' );
+			const content = tab.querySelector( '.wp-block-themeisle-blocks-tabs-item-content' );
+
+			header.addEventListener( 'click', () => {
+				tabs.forEach( other => {
+					const otherHeader = other.querySelector( '.wp-block-themeisle-blocks-tabs-item-header' );
+					const otherContent = other.querySelector( '.wp-block-themeisle-blocks-tabs-item-content' );
+					setTabStatus( otherContent, false );
+					otherHeader?.classList.remove( 'active' );
+				});
+				setTabStatus( content, true );
+				header.classList.add( 'active' );
+			});
+		});
+
+		activateFirstTab( headers, tabs );
+	});
+});
