e17aacd27b8050eb32ee32637c47c99f1a958b32
S. Robert <soarerobert17@gmail.com>
2021-07-21 09:45:21 +0000

    Add Neve Pro condition

--- src/plugins/wc-integration/index.js
+++ src/plugins/wc-integration/index.js
@@ -9,38 +9,50 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { Fragment, useEffect } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
 import { select } from '@wordpress/data';
-import { COLLECTIONS_STORE_KEY } from '@woocommerce/block-data';
-
 import { extractProductsData } from './utility';
 import SelectProducts from './SelectProduct';
 
+const wcBlocksData = import( '@woocommerce/block-data' );
+
 const PRODUCTS_PER_PAGE = 100;
+const MAX_PRODUCT_REQUESTS = 30;
 
 const sortByName = ( a, b ) => {
 	return a.name.localeCompare( b.name );
 };
 
-let wcProductData = select( COLLECTIONS_STORE_KEY ).getCollection( '/wc/store', 'products', { per_page: PRODUCTS_PER_PAGE, orderBy: 'title' }).sort( sortByName );
+let wcProductData = [];
 let countRetrive = 0;
+let wcLoaded = false;
 
-/**
- * If the list of products is still empty, make a request once a second.
- * After 30 tries, delete de interval
- */
-const retrieveData = setInterval( () => {
-	if ( 0 < wcProductData?.length || 30 < countRetrive ) {
-		clearInterval( retrieveData );
-	} else {
-		wcProductData = select( COLLECTIONS_STORE_KEY ).getCollection( '/wc/store', 'products', { per_page: PRODUCTS_PER_PAGE, orderBy: 'title' }).sort( sortByName );
-		countRetrive += 1;
-	}
-}, 1000 );
+wcBlocksData?.then( ( blocksData ) => {
+	const { COLLECTIONS_STORE_KEY } = blocksData;
+	wcProductData = select( COLLECTIONS_STORE_KEY )?.getCollection( '/wc/store', 'products', { per_page: PRODUCTS_PER_PAGE, orderBy: 'title' }).sort( sortByName );
+
+	/**
+	 * If the list of products is still empty, make a request once a second.
+	 * After 30 tries, delete de interval
+	 */
+	const retrieveData = setInterval( () => {
+		if ( MAX_PRODUCT_REQUESTS > countRetrive ) {
+			console.warn( __( '[Otter][Review-WC] Products are unavailable!' ), 'otter-blocks' );
+		}
+		if ( 0 < wcProductData?.length || MAX_PRODUCT_REQUESTS < countRetrive ) {
+			clearInterval( retrieveData );
+		} else {
+			wcProductData = select( COLLECTIONS_STORE_KEY ).getCollection( '/wc/store', 'products', { per_page: PRODUCTS_PER_PAGE, orderBy: 'title' }).sort( sortByName );
+			countRetrive += 1;
+		}
+	}, 1000 );
+
+	wcLoaded = true;
+});
 
 
 const withWooCommerceIntegrationExtension = createHigherOrderComponent(
 	( BlockEdit ) => {
 		return ( props ) => {
-			if ( 'themeisle-blocks/review' === props.name ) {
+			if ( 'themeisle-blocks/review' === props.name && '1' === window?.themeisleGutenberg?.hasNevePro && wcLoaded ) {
 
 				const products = extractProductsData( wcProductData );
 				const selectedProduct = products?.filter( ({ id }) => id === props?.attributes?.postId )[0] || undefined;
